20bf2bd20a1bcebd283019166db7a02b5038a671
legitscrub <jobowman@utexas.edu>
2022-03-28 15:45:08 -0500

    green start

--- code/green_certification.R
+++ code/green_certification.R
@@ -1,3 +1,17 @@
 # green certification
 
+library(tidyverse)
+library(ggplot2)
+library(rpart)
+library(rpart.plot)
+library(randomForest)
+library(rsample)
+library(modelr)
+library(fastDummies)
+
 onlygreens <- read.csv(file.path(path, 'data/greenbuildings.csv'))
+
+view(greenbuildings)
+
+sapply(greenbuildings, function(x) sum(is.na(x)))
+greenbuildings = na.exclude(greenbuildings)
