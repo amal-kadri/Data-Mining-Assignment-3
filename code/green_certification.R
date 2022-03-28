a85dda5deca4ecc31805d36094bc41fa99e14860
legitscrub <jobowman@utexas.edu>
2022-03-28 17:17:49 -0500

    important variables

--- code/green_certification.R
+++ code/green_certification.R
@@ -10,6 +10,7 @@ library(modelr)
 library(fastDummies)
 library(here)
 library(glmnet)
+library(gbm)
 path <-  here()
 
 onlygreens <- read.csv(file.path(path, 'data/greenbuildings.csv'))
@@ -17,7 +18,6 @@ view(onlygreens)
 
 # generate relevant variables
 onlygreens = onlygreens %>% mutate(
-  green_cert = ifelse(LEED == 1 | Energystar == 1, 1, 0),
   rent_psf = Rent * leasing_rate
 )
 
@@ -32,23 +32,23 @@ green_test = testing(green_split)
 
 #lasso model w/ comp
 greenY <- green_train$rent_psf
-greenX <- model.matrix(rent_psf ~ (.-CS_PropertyID - cluster - amenities)^2 -1, data = green_train)
+greenX <- model.matrix(rent_psf ~ (.-CS_PropertyID - cluster - amenities- LEED - Energystar)^2 -1, data = green_train)
 
 greenLasso_Wcomp <- cv.glmnet(x = greenX,y = greenY ,alpha = 1, nfold = 20, trace.it = 1, standardize = FALSE)
 
-green_coef_comp = coef(greenLasso) %>% 
+green_coef_Wcomp = coef(greenLasso_Wcomp) %>% 
   as.matrix() %>% 
   as.data.frame()
-green_coef_comp = green_coef %>% 
+green_coef_Wcomp = green_coef_Wcomp %>% 
   mutate(mag = abs(s1)) %>% 
   filter(mag > 0)
-green_coef_comp <- tibble::rownames_to_column(green_coef_comp, "VALUE")
-green_coef_comp = green_coef_comp[2:nrow(green_coef_comp),1]
-green_coef_comp
+green_coef_Wcomp <- tibble::rownames_to_column(green_coef_Wcomp, "VALUE")
+green_coef_Wcomp = green_coef_Wcomp[2:nrow(green_coef_Wcomp),1]
+green_coef_Wcomp
 
 #lasso model w/o Rent and leasing_rate
 greenY <- green_train$rent_psf
-greenX <- model.matrix(rent_psf ~ (.-CS_PropertyID - cluster - amenities - Rent - leasing_rate)^2 -1, data = green_train)
+greenX <- model.matrix(rent_psf ~ (.-CS_PropertyID - cluster - amenities - Rent - leasing_rate - LEED - Energystar)^2 -1, data = green_train)
 
 greenLasso <- cv.glmnet(x = greenX,y = greenY ,alpha = 1, nfold = 20, trace.it = 1, standardize = FALSE)
 
@@ -67,11 +67,29 @@ green_forest = randomForest(rent_psf ~ (.- CS_PropertyID - LEED - Energystar - R
                                  data = green_train, importance = TRUE, 
                                  na.action = na.exclude)
 modelr::rmse(green_forest, green_test)
-modelr::rmse(green_forest, green_test)
 sqrt(greenLasso$cvm[greenLasso$lambda == greenLasso$lambda.min])
 
-partialPlot(green_forest, green_test, 'green_cert', las=1)
-# partialPlot(green_forest, green_test, 'precipitation_amt', las=1)
-# partialPlot(green_forest, green_test, 'city_num', las=1)
+yhat_test = predict(green_forest, green_test)
+plot(yhat_test, green_test$rent_psf)
+
+# boost
+boost1 = gbm(rent_psf ~ (.- CS_PropertyID - LEED - Energystar - Rent - leasing_rate), 
+             data = green_train,
+             interaction.depth=5, n.trees=500, shrinkage=.05)
+
+# Look at error curve -- stops decreasing much after ~300
+gbm.perf(boost1)
 
 
+yhat_test_gbm = predict(boost1, green_test, n.trees=200)
+
+#rmse boost
+rmse(boost1, green_test)
+
+varImpPlot(green_forest)
+
+partialPlot(green_forest, green_test, 'age', las=1)
+partialPlot(green_forest, green_test, 'size', las=1)
+partialPlot(green_forest, green_test, 'stories', las=1)
+partialPlot(green_forest, green_test, 'City_Market_Rent', las=1)
+partialPlot(green_forest, green_test, 'green_rating', las=1)
