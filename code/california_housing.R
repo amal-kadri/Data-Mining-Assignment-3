642d5dfae544a88ff8deb761500fe97bdcaf6a0d
dascolari <dascolari@utexas.edu>
2022-04-05 22:23:15 -0500

    cal housing

--- code/california_housing.R
+++ code/california_housing.R
@@ -21,40 +21,36 @@ calhousing_forest = randomForest(medianHouseValue ~ . - totalRooms - totalBedroo
                             na.action = na.exclude)
 
 # out of sample performance measure: RMSE
-modelr::rmse(calhousing_forest, calhousing_test)
+ch_forest <- modelr::rmse(calhousing_forest, calhousing_test)
 
 # make predictions plot to show journey of thought
 # seems like the model does worse at predicting the most expensive houses.
 # and better at predicting lower price houses
 yhat_test = predict(calhousing_forest, calhousing_test)
-plot(yhat_test, calhousing_test$medianHouseValue)
-
-calhousing = calhousing %>% 
-  mutate(yhat_forest = predict(calhousing_forest, calhousing),
-         abs_diff = abs(yhat_test-medianHouseValue))
+howwell_chforest <- plot(yhat_test, calhousing_test$medianHouseValue)
 
 
 #var import
-varImpPlot(calhousing_forest)
+chf_imp <- varImpPlot(calhousing_forest)
 
 #partial deps
-partialPlot(calhousing_forest, calhousing_test, 'longitude', las=1)
-partialPlot(calhousing_forest, calhousing_test, 'latitude', las=1)
+pd_lng <- partialPlot(calhousing_forest, calhousing_test, 'longitude', las=1)
+pd_lat <- partialPlot(calhousing_forest, calhousing_test, 'latitude', las=1)
 
 # operation "4th Booster"
 # dev a gradient boosted tree based model of med house prices
 calhousing_boost = gbm(medianHouseValue ~ . - totalRooms - totalBedrooms, 
              data = calhousing_train,
-             interaction.depth=5, n.trees=500, shrinkage=.05)
+             interaction.depth=5, n.trees=500, shrinkage=.05, cv.folds = 10)
 
-gbm.perf(calhousing_boost)
-
-
-yhat_test_gbm = predict(cal_boost, calhousing_test, n.trees=200)
-yhat_test_gbm = predict(cal_boost, calhousing, n.trees=200)
+booster_unboosted <- gbm.perf(calhousing_boost)
 
 
-rmse(calhousing_boost, calhousing_test)
+yhat_test_gbm = predict(calhousing_boost, calhousing_test, n.trees=200)
+yhat_test_gbm = predict(calhousing_boost, calhousing, n.trees=200)
+
+
+booster_unboosted_rmse <- rmse(calhousing_boost, calhousing_test)
 
 # trying to boost the boosted 
 ctrl <- trainControl(
@@ -69,7 +65,7 @@ tuneGrid <- expand.grid(
   n.minobsinnode = 10
 )
 
-model5 <- train(
+calhousing_boostest <- train(
   medianHouseValue ~ .- totalRooms - totalBedrooms,
   data = calhousing_train,
   method = 'gbm',
@@ -78,39 +74,21 @@ model5 <- train(
   verbose = F
 )
 
-plot(model5)
+plot(calhousing_boostest)
 
 test.features = subset(calhousing_test, select=-c(medianHouseValue))
 test.target = subset(calhousing_test, select=medianHouseValue)[,1]
 
-predictions = predict(model5, newdata = test.features)
+predictions = predict(calhousing_boostest, newdata = test.features)
 
 #gradient boosted RMSE
-sqrt(mean((test.target - predictions)^2))
+ch_boostest <- modelr::rmse(calhousing_boostest, calhousing_test)
 
-# I propose we axe the utopia model as lat long are important, 
-# vis a vie the varimpmat(cal_forest)
-# utpoia model
-utopia_forest = randomForest(medianHouseValue ~ . - totalRooms - totalBedrooms - longitude - latitude,
-                                 data = calhousing_train, importance = TRUE, 
-                                 na.action = na.exclude)
-modelr::rmse(utopia_forest, calhousing_test)
-yhat_test = predict(utopia_forest, calhousing_test)
-plot(yhat_test, calhousing_test$medianHouseValue)
-
-calhousing_test = calhousing_test %>% 
-  mutate(yhat_test = predict(utopia_forest, calhousing_test),
-         abs_diff = abs(yhat_test-medianHouseValue))
-
+## MAPS
 
 calhousing = calhousing %>% 
-  mutate(yhat = predict(utopia_forest, calhousing),
+  mutate(yhat_forest = predict(calhousing_forest, calhousing),
          abs_diff = abs(yhat_test-medianHouseValue))
-
-
-varImpPlot(utopia_forest)
-
-# sqrt(calhousingLasso$cvm[calhousingLasso$lambda == calhousingLasso$lambda.min])
 
 # a plot of the original data, using a color scale to show medianHouseValue (or log medianHouseValue) versus longitude (x) and latitude (y)
 # color hex codes: https://htmlcolorcodes.com/
@@ -128,7 +106,8 @@ medpirce_map <- cali + geom_point(data = calhousing,
                   aes(x = longitude, 
                            y = latitude, 
                            color = medianHouseValue)) +
-  scale_color_gradientn(colors = c('#FFFFFF', '#00A6FF', '#014468'))
+  scale_color_gradientn(colors = c('#FFFFFF', '#00A6FF', '#014468')) +
+  labs(title = "California Houses: Median Prices")
 
 # map for predicted house meds
 # as noted above, compare color to real data map
@@ -138,7 +117,10 @@ yhat_map <- cali + geom_point(data = calhousing,
                   aes(x = longitude, 
                       y = latitude, 
                       color = yhat_forest)) +
-  scale_color_gradientn(colors = c('#014468', '#00A6FF', '#FFFFFF'))
+  scale_color_gradientn(colors = c('#014468', '#00A6FF', '#FFFFFF')) +
+  labs(title = "California Houses: Estimated Median Prices") + 
+  theme(legend.text = "Median House Price Estimate")
+
 # map of abs_diff
 # color needs work , perhaps a interval shift, pin the low part of the gradient at a smaller number
 # since the predications are all relatively the same in accuracy, there's not a whole lotta 
@@ -149,4 +131,5 @@ absdiff_map <- cali + geom_point(data = calhousing,
                   aes(x = longitude, 
                       y = latitude, 
                       color = abs_diff)) +
-  scale_color_gradientn(colors = c('#17B417', '#F2F52A', '#DF4E14'))
+  scale_color_gradientn(colors = c('#17B417', '#F2F52A', '#DF4E14')) +
+  labs(title = "California Houses: Best Median House Price Model Error")
